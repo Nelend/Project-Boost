11477eb93ecb18b17a77a96c3dc82f15cbde406f
Nelend <guy-k@mail.ru>
2023-05-12 12:51:40 +0300

    mod Rocket

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+    float force = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-
+            rigidBody.AddRelativeForce(Vector3.up * force);
         }
 
         if (Input.GetKey(KeyCode.A))
