6f1f22c2575ad9eaeb0115afbcc6abcf62789470
Nelend <guy-k@mail.ru>
2023-05-23 15:48:29 +0300

    add Oscillator.cs

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(0, -8f, 0);
+    [SerializeField] float peroid = 2f;
+
+    float movementFactor;
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / peroid;
+
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
